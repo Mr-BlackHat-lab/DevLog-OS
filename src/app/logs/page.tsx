8405ac79bbab371dadd57f07f8f95cd3b32ba8c5
Prahalad Singh <prahalad.hat@outlook.com>
2025-12-22 13:18:36 +0530

    markdown added in logs next is filter by mood

--- src/app/logs/page.tsx
+++ src/app/logs/page.tsx
@@ -3,6 +3,14 @@ import LogForm from '@/components/LogForm';
 import '@/styles/form.css'
 import '@/styles/page.css';
 import '@/styles/logs.css';
+import '@/styles/markdown.css'
+
+// import of markdown concept
+import ReactMarkdown from 'react-markdown'
+import remarkGfm from 'remark-gfm';
+import rehypeHighlight from 'rehype-highlight';
+import { Content } from 'next/font/google';
+
 
 async function getLogs() {
     const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
@@ -35,7 +43,14 @@ export default async function LogsPage() {
                         <div className="log-meta">
                             {new Date(log.createdAt).toLocaleString()}.{log.mood}
                         </div>
-                        <div className="log-content">{log.content}</div>
+                        <div className="log-content markdown">
+                            <ReactMarkdown
+                                remarkPlugins={[remarkGfm]}
+                                rehypePlugins={[rehypeHighlight]}
+                            >
+                                {log.content}
+                            </ReactMarkdown>
+                        </div>
                     </div>
                 ))}
             </div>
